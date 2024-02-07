ea0ddd8f5f9edf66e4c2ea43f4657bb449253a37
Violet-Riah <violettanjombwa@gmail.com>
2024-02-07 22:26:13 +0300

    add one file

---
+++ Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    var name="violet njombwa"
+    println(name)
+    var age=20
+    println(age)
+    var number= +265887147210
+    println(number)
+    var weight=50
+    println(weight)
+    var nationality="Malawian"
+    println(nationality)
+    val citizen=true
+    val notcitizen=false
+    println(citizen)
+
+
+
+}
